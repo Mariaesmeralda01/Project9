785617c7ea99ceea1953e60eb5fc9c96dd80a093
Esesm <Esesm@LAPTOP-JL4SL9QM.home>
2023-07-28 23:19:07 -0400

    Initial commit

---
+++ src/demo8/DemoTest9.java
@@ -0,0 +1,17 @@
+package demo8;
+
+public class DemoTest9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 0;
+while (a < 10) {
+	System.out.println("a is less than 10: " + a);
+	a++;
+	
+	}
+
+  }
+
+}
